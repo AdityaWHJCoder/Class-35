fe916daa1044f02b27e6b16e3fe856c6a9c7d513
AdityaWHJCoder <72691990+AdityaWHJCoder@users.noreply.github.com>
2020-12-24 15:56:25 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,30 @@
+class Form {
+    constructor(){}
+    
+    display(){
+      var title = createElement('h2');
+      title.html("Car Racing Game");
+      title.position(530, 50);
+
+      var input = createInput("Name");
+      input.position(530, 260);
+
+      var button = createButton("Play");
+      button.position(550, 300);
+
+      var greeting = createElement('h3');
+      button.mousePressed(function(){
+       input.hide();
+       button.hide();
+
+       var name = input.value();
+       playerCount = playerCount+1;
+       player.update(name);
+       player.updateCount(playerCount);
+
+       greeting.html("Hello "+ name);
+       greeting.position(530,160);
+      });
+    }
+
+}
